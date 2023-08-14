97d3513baf1b8830af60cb8864030cd9a09159ac
Ian McBee <mcbee_ian@bah.com>
2023-08-13 23:26:32 -0400

    rename and restore

---
+++ practice-app-2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // this is where all of the <app-root> is generate in the html file
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
---
+++ practice-app-2/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TaskbarComponent } from './taskbar/taskbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    TaskbarComponent,
+    FooterComponent,
+    WarningalertComponent,
+    SuccessalertComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
