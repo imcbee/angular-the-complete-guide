b66092f4b35a7d4cf3418cec984e5967ac2572ec
Ian McBee <mcbee_ian@bah.com>
2023-08-22 18:30:01 -0400

    completed forms assignment

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  signupForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      "userData": new FormGroup({
+        "name": new FormControl(null, [Validators.required, CustomValidators.invalidProjectName, CustomValidators.asyncInvalidProjectName]),
+        "email": new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      "status": new FormControl('critical')
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+
+    this.signupForm.reset();
+  }
+
+  
 }
