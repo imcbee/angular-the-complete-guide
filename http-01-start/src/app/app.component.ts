55221f0cf63d7b0fffc8b4c5509d3a82ca430fc9
Ian McBee <mcbee_ian@bah.com>
2023-08-28 21:05:41 -0400

    develop http logic and error handling

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +10,49 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private postService: PostService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isFetching =  true;
+    this.postService.fetchPosts().subscribe( posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      console.log(error);
+      this.error = error.message;
+    });
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
+    //console.log(postData);
     // Send Http request
-    console.log(postData);
+    this.postService.createAndStorePost(postData.title, postData.content);
+    
   }
 
   onFetchPosts() {
     // Send Http request
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      console.log(error);
+      this.error = error.message;
+    });
   }
 
   onClearPosts() {
     // Send Http request
+    this.postService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
+
 }
