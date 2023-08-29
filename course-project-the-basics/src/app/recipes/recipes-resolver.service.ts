b9cfe7cb2a242c8bccb58c36d716d0bd04379adc
Ian McBee <mcbee_ian@bah.com>
2023-08-29 14:26:23 -0400

    fig bug

--- course-project-the-basics/src/app/recipes/recipes-resolver.service.ts
+++ course-project-the-basics/src/app/recipes/recipes-resolver.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 
-import { Recipe } from "./recipe.model";
-import { DataStorageService } from "../shared/data-storage.service";
 import { Observable } from "rxjs";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipeResolverService implements Resolve<Recipe[]> {
     constructor(
         private dataStorageServicer: DataStorageService,
+        private receipeService: RecipeService
     ) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-        return this.dataStorageServicer.fetchRecipes();
+        const recipes = this.receipeService.getRecipes();
+
+        if (recipes.length === 0) {
+            return this.dataStorageServicer.fetchRecipes();
+        } else {
+            return recipes;
+        }
     }
 }
