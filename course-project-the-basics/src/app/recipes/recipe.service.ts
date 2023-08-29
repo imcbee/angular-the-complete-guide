22ed457091560e00ebb74d88096b1c894dd0f69a
Ian McBee <mcbee_ian@bah.com>
2023-08-28 22:48:48 -0400

    Added http function with firevase backend

--- course-project-the-basics/src/app/recipes/recipe.service.ts
+++ course-project-the-basics/src/app/recipes/recipe.service.ts
@@ -10,42 +10,47 @@ import { Subject } from 'rxjs';
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Hamburger',
-      'Make a hamburger',
-      'https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/1:1/w_1920,c_limit/the-ultimate-hamburger.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Lettuce', 2),
-        new Ingredient('Tomato', 1),
-        new Ingredient('Mustard', 1),
-        new Ingredient('Pickles', 3),
-      ]
-    ),
-    new Recipe(
-      'French Fries',
-      'Make a french fry',
-      'https://www.allrecipes.com/thmb/JZsbJ8XNPxkbDk_hwHz-KI7nSKk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/219634-chef-johns-french-fries-DDMFS-4x3-338dec7976fc4d75bb3e2c18ff6a6e95.jpg',
-      [
-        new Ingredient('Potatoes', 4),
-        new Ingredient('Salt', 1),
-        new Ingredient('Oil', 1),
-      ]
-    ),
-    new Recipe(
-      'Milkshake',
-      'make a milkshake',
-      'https://preppykitchen.com/wp-content/uploads/2021/04/Milkshake-Recipe-Card.jpg',
-      [
-        new Ingredient('Milk', 1),
-        new Ingredient('Icecream', 1),
-      ]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Hamburger',
+  //     'Make a hamburger',
+  //     'https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/1:1/w_1920,c_limit/the-ultimate-hamburger.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Lettuce', 2),
+  //       new Ingredient('Tomato', 1),
+  //       new Ingredient('Mustard', 1),
+  //       new Ingredient('Pickles', 3),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'French Fries',
+  //     'Make a french fry',
+  //     'https://www.allrecipes.com/thmb/JZsbJ8XNPxkbDk_hwHz-KI7nSKk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/219634-chef-johns-french-fries-DDMFS-4x3-338dec7976fc4d75bb3e2c18ff6a6e95.jpg',
+  //     [
+  //       new Ingredient('Potatoes', 4),
+  //       new Ingredient('Salt', 1),
+  //       new Ingredient('Oil', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Milkshake',
+  //     'make a milkshake',
+  //     'https://preppykitchen.com/wp-content/uploads/2021/04/Milkshake-Recipe-Card.jpg',
+  //     [
+  //       new Ingredient('Milk', 1),
+  //       new Ingredient('Icecream', 1),
+  //     ]
+  //   ),
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
