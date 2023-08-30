6960a24c67dd3002988c98c7993312be904ea5af
Ian McBee <mcbee_ian@bah.com>
2023-08-29 20:26:42 -0400

    set up auth and loading spinner

--- course-project-the-basics/src/app/app-routing.module.ts
+++ course-project-the-basics/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { AuthComponent } from './auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeResolverService } from './recipes/recipes-resolver.service';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
     {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService]},
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
   // { path: '**', component: RecipesComponent },
 ];
 
---
+++ course-project-the-basics/src/app/auth/auth.component.ts
@@ -0,0 +1,52 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { AuthService } from "./auth.service";
+
+@Component({
+    selector: 'app-auth',
+    templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+    isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+
+    constructor(
+        private authService: AuthService,
+    ) {}
+
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode; // this is better to switch from true to false
+    }
+
+    onSubmit(form: NgForm) {
+        //console.log(form.value);
+        if (!form.valid) {
+            return;
+        }
+
+        const email = form.value.email;
+        const password = form.value.password;
+
+        this.isLoading = true;
+        if (this.isLoginMode) {
+            //...
+        } else {
+            this.authService.signUp(email, password)
+                .subscribe(resData => {
+                    console.log(resData);
+                    this.isLoading = false;
+                },
+                errorMessage => {
+                    console.log(errorMessage);
+                    this.error = errorMessage;
+                    this.isLoading = false;
+                }
+            );
+        }
+
+
+        form.reset();
+    }
+}
