6821659dad519782505465521c870e86c5eec5da
Ian McBee <mcbee_ian@bah.com>
2023-09-05 18:02:53 -0400

    Comment.

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -3,9 +3,13 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, //{
+  //providers: [AnalyticsService] //! this is like having a providers array in app.module
+//}
+);
